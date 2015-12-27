ea0b27bdadae10bae0eecc333bef63b7406a6bd7
Thomas Laurenson <thomas@thomaslaurenson.com>
2015-12-28 08:52:49 +1300

    Upload required libraries.

---
+++ dfxml/copy_dfxml_files.sh
@@ -0,0 +1,10 @@
+# This script copies new versions of dfxml to current directory
+
+echo ">>> Copying dfxml.py"
+cp ../../dfxml/python/dfxml.py dfxml.py
+
+echo ">>> Copying fiwalk.py"
+cp ../../dfxml/python/fiwalk.py fiwalk.py
+
+echo ">>> Copying Objects.py"
+cp ../../dfxml/python/Objects.py Objects.py
