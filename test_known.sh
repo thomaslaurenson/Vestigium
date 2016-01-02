44a7385b7c56446916b756b4e26344cdd32ae510
Thomas Laurenson <thomas@thomaslaurenson.com>
2016-01-03 11:41:14 +1300

    Updated test cases for known data set testing.

--- test_known.sh
+++ test_known.sh
@@ -2,19 +2,28 @@
 # Known data sets
 
 # TRUECRYPT
-python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC-7-32/1-install.raw output/TCinstall /media/forensic/PHD/application_profiles/apxmls_demonstration/data_final/TrueCrypt-7.1a-6.1.7601-FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/TC-7-32/1-install.xml --hives /media/forensic/PHD/known_data_sets/TC-7-32/1-install/
+python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC-7-32/1-install.raw output/TC-1-install /media/forensic/PHD/application_profiles/apxmls_demonstration/data_final/TrueCrypt-7.1a-6.1.7601-FINAL-NORM.apxml --dfxml /media/forensic/PHD/known_data_sets/TC-7-32/1-install.xml --hives /media/forensic/PHD/known_data_sets/TC-7-32/1-install/
 
-python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC-7-32/2-open.raw output/TCopen /media/forensic/PHD/application_profiles/apxmls_demonstration/data_final/TrueCrypt-7.1a-6.1.7601-FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/TC-7-32/2-open.xml --hives /media/forensic/PHD/known_data_sets/TC-7-32/2-open/
+python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC-7-32/2-open.raw output/TC-2-open /media/forensic/PHD/application_profiles/apxmls_demonstration/data_final/TrueCrypt-7.1a-6.1.7601-FINAL-NORM.apxml --dfxml /media/forensic/PHD/known_data_sets/TC-7-32/2-open.xml --hives /media/forensic/PHD/known_data_sets/TC-7-32/2-open/
 
-python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC-7-32/3-close.raw output/TCclose /media/forensic/PHD/application_profiles/apxmls_demonstration/data_final/TrueCrypt-7.1a-6.1.7601-FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/TC-7-32/3-close.xml --hives /media/forensic/PHD/known_data_sets/TC-7-32/3-close/
+python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC-7-32/3-close.raw output/TC-3-close /media/forensic/PHD/application_profiles/apxmls_demonstration/data_final/TrueCrypt-7.1a-6.1.7601-FINAL-NORM.apxml --dfxml /media/forensic/PHD/known_data_sets/TC-7-32/3-close.xml --hives /media/forensic/PHD/known_data_sets/TC-7-32/3-close/
 
-python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC-7-32/4-uninstall.raw output/TCuninstall /media/forensic/PHD/application_profiles/apxmls_demonstration/data_final/TrueCrypt-7.1a-6.1.7601-FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/TC-7-32/4-uninstall.xml --hives /media/forensic/PHD/known_data_sets/TC-7-32/4-uninstall/
+python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC-7-32/4-uninstall.raw output/TC-4-uninstall /media/forensic/PHD/application_profiles/apxmls_demonstration/data_final/TrueCrypt-7.1a-6.1.7601-FINAL-NORM.apxml --dfxml /media/forensic/PHD/known_data_sets/TC-7-32/4-uninstall.xml --hives /media/forensic/PHD/known_data_sets/TC-7-32/4-uninstall/
 
-# TRUECRYPT: NEW TEST CASES
-python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC/TC-1-install.raw output/TC-1-installnew /media/forensic/PHD/application_profiles/apxmls_demonstration/data_final/TrueCrypt-7.1a-6.1.7601-FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/TC/TC-1-install.xml --hives /media/forensic/PHD/known_data_sets/TC/TC-1-install/
+# TRUECRYPT: REVISED
+python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC/TC-1-install.raw output/TC-1-install-new /media/forensic/PHD/github/apxml/TrueCrypt-7.1a-6.1.7601_FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/TC/TC-1-install.xml --hives /media/forensic/PHD/known_data_sets/TC/TC-1-install/
 
-python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC/TC-2-open.raw output/TC-2-opennew /media/forensic/PHD/application_profiles/apxmls_demonstration/data_final/TrueCrypt-7.1a-6.1.7601-FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/TC/TC-2-open.xml --hives /media/forensic/PHD/known_data_sets/TC/TC-2-open/
+python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC/TC-2-open.raw output/TC-2-open-new /media/forensic/PHD/github/apxml/TrueCrypt-7.1a-6.1.7601_FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/TC/TC-2-open.xml --hives /media/forensic/PHD/known_data_sets/TC/TC-2-open/
 
-python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC/TC-3-close.raw output/TC-3-closenew /media/forensic/PHD/application_profiles/apxmls_demonstration/data_final/TrueCrypt-7.1a-6.1.7601-FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/TC/TC-3-close.xml --hives /media/forensic/PHD/known_data_sets/TC/TC-3-close/
+python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC/TC-3-close.raw output/TC-3-close-new /media/forensic/PHD/github/apxml/TrueCrypt-7.1a-6.1.7601_FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/TC/TC-3-close.xml --hives /media/forensic/PHD/known_data_sets/TC/TC-3-close/
 
-python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC/TC-4-uninstall.raw output/TC-4-uninstallnew /media/forensic/PHD/application_profiles/apxmls_demonstration/data_final/TrueCrypt-7.1a-6.1.7601-FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/TC/TC-4-uninstall.xml --hives /media/forensic/PHD/known_data_sets/TC/TC-4-uninstall/
+python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/TC/TC-4-uninstall.raw output/TC-4-uninstall-new /media/forensic/PHD/github/apxml/TrueCrypt-7.1a-6.1.7601_FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/TC/TC-4-uninstall.xml --hives /media/forensic/PHD/known_data_sets/TC/TC-4-uninstall/
+
+# CCLEANER
+python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/CC-7-32/1-install.raw output/CC-1-install /media/forensic/PHD/github/apxml/CCleaner-509-6.1.7601-FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/CC-7-32/1-install.xml --hives /media/forensic/PHD/known_data_sets/CC-7-32/1-install/
+
+python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/CC-7-32/2-open.raw output/CC-2-open /media/forensic/PHD/github/apxml/CCleaner-509-6.1.7601-FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/CC-7-32/2-open.xml --hives /media/forensic/PHD/known_data_sets/CC-7-32/2-open/
+
+python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/CC-7-32/3-close.raw output/CC-3-close /media/forensic/PHD/github/apxml/CCleaner-509-6.1.7601-FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/CC-7-32/3-close.xml --hives /media/forensic/PHD/known_data_sets/CC-7-32/3-close/
+
+python3.4 Vestigium.py /media/forensic/PHD/known_data_sets/CC-7-32/4-uninstall.raw output/CC-4-uninstall /media/forensic/PHD/github/apxml/CCleaner-509-6.1.7601-FINAL.apxml --dfxml /media/forensic/PHD/known_data_sets/CC-7-32/4-uninstall.xml --hives /media/forensic/PHD/known_data_sets/CC-7-32/4-uninstall/
